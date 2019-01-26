e41b34f3363c942da82bdd86fd616979dee60111
nithinmic <nithinmic@gmail.com>
2019-01-26 16:41:07 +0530

    added integer value

--- odd.py
+++ odd.py
@@ -1,6 +1,10 @@
-a=int(input("Enter a Number "))
-if a%2==0:
-	print(a,"is even!")
-else:
-	print(a,"is odd!")
+a=input("Enter a Number ")
+try:
+	a=int(a)
+	if a%2==0:
+		print(a,"is even!")
+	else:
+		print(a,"is odd!")
+except ValueError:
+	print("Input is not a Number")	
 
